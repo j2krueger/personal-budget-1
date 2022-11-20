9da4de50a5024b9350dbc5ef1d2f810f5774aae4
John Krueger <j_krueger@yahoo.com>
2022-11-20 16:50:01 -0500

    Set up server to be restarted easily

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Set up the server to be restarted easily
 app.get('/', (req, res, next) => {
-    res.status(200).send('Hello World! <a href="/stop">Click here to stop!</a>');
+    res.status(200).send('Hello World! <a href="/stop">Click here to stop server!</a>');
 });
 
 app.get('/stop', (req, res, next) => {
@@ -13,5 +14,5 @@ app.get('/stop', (req, res, next) => {
 })
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT} at time ${new Date().toString()}`);
 });
